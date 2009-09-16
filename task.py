6b2f321fa6d384119339e72fbd89ab21fa917251
Alan Sparrow <alan@freeflight.org.uk>
2009-09-16 16:19:03 +0000

    Initial hildon-isation

--- task.py
+++ task.py
@@ -1,26 +1,44 @@
 #!/usr/bin/env python
 
-import freedb
 import gtk
 import gobject
 from pysqlite2 import dbapi2 as sqlite
+is_hildon_app = True
+try:
+    import hildon
+except ImportError:
+    is_hildon_app = False
+
+import freedb
 import projection
 
 TASKS = 'ABCDEFGH'
 NUM_TASKS = len(TASKS)
 
-class TaskApp:
+if is_hildon_app:
+    AppBase = hildon.Program
+else:
+    AppBase = object
+
+class TaskApp(AppBase):
     def __init__(self):
+        AppBase.__init__(self)
+
         self.task_db = freedb.Freedb()
         self.lambert = projection.Lambert(*self.task_db.get_projection())
-
         self.task_index, self.tasks = self.load_tasks()
 
-        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        # Create window and add event handlers
+        if is_hildon_app:
+            self.window = hildon.Window()
+        else:
+            self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
         self.window.set_title('Task')
         self.window.set_border_width(3)
         self.window.connect('destroy', gtk.main_quit)
         self.window.connect('delete_event', self.quit)
+        self.window.connect('key-press-event', self.on_keypress)
+        self.window.connect('window-state-event', self.on_window_state_change)
 
         # Create waypoint list
         wp_store = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_STRING)
@@ -38,6 +56,7 @@ class TaskApp:
         col2.set_sort_column_id(1)
 
         wp_view = gtk.TreeView(wp_store)
+        wp_view.set_headers_visible(True)
         wp_view.append_column(col1)
         wp_view.append_column(col2)
         wp_window = gtk.ScrolledWindow()
@@ -53,12 +72,12 @@ class TaskApp:
         col.pack_start(cell, True)
         col.add_attribute(cell, 'text', 0)
 
-        task_view = gtk.TreeView(self.task_store)
-        self.task_view = task_view
-        task_view.set_headers_visible(False)
-        task_view.set_reorderable(True)
-        task_view.append_column(col)
-        task_view.connect('row-activated', self.wp_activated, self.task_store)
+        self.task_view = gtk.TreeView(self.task_store)
+        self.task_view.set_headers_visible(False)
+        self.task_view.set_reorderable(True)
+        self.task_view.append_column(col)
+        self.task_view.connect('row-activated', self.wp_activated,
+                               self.task_store)
 
         # Task distance
         self.dist_label = gtk.Label('')
@@ -68,6 +87,7 @@ class TaskApp:
         del_button = gtk.Button('Del')
         del_button.connect('clicked', self.delete_wp)
 
+        # Observation zone set...
         oz_button = gtk.Button('OZ...')
         oz_button.connect('clicked', self.set_oz)
 
@@ -85,7 +105,7 @@ class TaskApp:
         # Packing
         vbox = gtk.VBox()
         vbox.set_spacing(5)
-        vbox.pack_start(task_view, expand=True)
+        vbox.pack_start(self.task_view, expand=True)
         vbox.pack_start(self.dist_label, expand=False)
         vbox.pack_start(del_button, expand=False)
         vbox.pack_start(oz_button, expand=False)
@@ -101,36 +121,43 @@ class TaskApp:
 
         # Drag and drop
         wp_view.enable_model_drag_source(gtk.gdk.BUTTON1_MASK,
-            [('text/plain', 0, 0)],
-            gtk.gdk.ACTION_DEFAULT | gtk.gdk.ACTION_COPY)
-        wp_view.connect('drag-data-get', self.drag_data_get)
+            [('WP_MODEL_ROW', gtk.TARGET_SAME_APP, 0)],
+            gtk.gdk.ACTION_COPY)
+        wp_view.connect('drag-data-get', self.wp_drag_data_get)
 
-        task_view.enable_model_drag_source(gtk.gdk.BUTTON1_MASK,
-            [('MY_TREE_MODEL_ROW', gtk.TARGET_SAME_WIDGET, 0),
-             ('text/plain', 0, 0)],
-            gtk.gdk.ACTION_DEFAULT | gtk.gdk.ACTION_MOVE)
-        task_view.enable_model_drag_dest(
-            [('MY_TREE_MODEL_ROW', gtk.TARGET_SAME_WIDGET, 0),
-             ('text/plain', 0, 0)],
+        self.task_view.enable_model_drag_source(gtk.gdk.BUTTON1_MASK,
+            [('TASK_MODEL_ROW', gtk.TARGET_SAME_WIDGET, 0)],
+            gtk.gdk.ACTION_COPY)
+        self.task_view.enable_model_drag_dest(
+            [('TASK_MODEL_ROW', gtk.TARGET_SAME_WIDGET, 0),
+             ('WP_MODEL_ROW', gtk.TARGET_SAME_APP, 0)],
             gtk.gdk.ACTION_DEFAULT)
-        task_view.connect('drag-data-get', self.drag_data_get)
-        task_view.connect('drag-data-received', self.drag_data_received)
+        self.task_view.connect('drag-data-get', self.task_drag_data_get)
+        self.task_view.connect('drag-data-received', self.drag_data_received)
 
         self.update_distance()
-        self.window.show_all()
 
-    def drag_data_get(self, view, context, selection, info, etime):
-        wpsel = view.get_selection()
-        model, iter = wpsel.get_selected()
+    def wp_drag_data_get(self, view, context, selection, info, etime):
+        sel = view.get_selection()
+        model, iter = sel.get_selected()
         id = model.get_value(iter, 0)
-        selection.set('text/plain', 8, id)
-        print "Get", id
+        selection.set('WP_DATA', 8, id)
 
-    def drag_data_received(self,
-                           task_view, context, x, y, selection, info, etime):
-        model = task_view.get_model()
+    def task_drag_data_get(self, view, context, selection, info, etime):
+        sel = view.get_selection()
+        model, paths = sel.get_selected_rows()
+        row = paths[0][0]
+        selection.set('TASK_DATA', 8, str(row))
+
+    def drag_data_received(self, view, context, x, y, selection, info, etime):
+        model = view.get_model()
         wp = selection.data
-        drop_info = task_view.get_dest_row_at_pos(x, y)
+        print "Received", wp, selection.type
+        if selection.type == 'TASK_DATA':
+            model.clear()
+            return
+
+        drop_info = view.get_dest_row_at_pos(x, y)
         if drop_info:
             path, position = drop_info
             iter = model.get_iter(path)
@@ -141,9 +168,6 @@ class TaskApp:
                 model.insert_after(iter, [wp])
         else:
              model.append([wp])
-
-        if context.action == gtk.gdk.ACTION_MOVE:
-            context.finish(True, True, etime)
 
         self.update_distance()
         self.task_saved = False
@@ -188,7 +212,6 @@ class TaskApp:
             tasks[i] = self.task_db.get_task(i)
 
         task_index = self.task_db.get_task_index()
-
         return (task_index, tasks)
 
     def save_task(self, button):
@@ -229,6 +252,19 @@ class TaskApp:
                 self.calc_distance(task[1:])
             return dist
 
+    def on_window_state_change(self, widget, event, *args):
+        if event.new_window_state & gtk.gdk.WINDOW_STATE_FULLSCREEN:
+            self.window_in_fullscreen = True
+        else:
+            self.window_in_fullscreen = False
+
+    def on_keypress(self, widget, event, *args):
+        if event.keyval == gtk.keysyms.F6:
+            if self.window_in_fullscreen:
+                self.window.unfullscreen()
+            else:
+                self.window.fullscreen()
+
     def quit(self, widget, event, data=None):
         if not self.task_saved:
             dialog = gtk.MessageDialog(buttons=gtk.BUTTONS_YES_NO,
@@ -238,9 +274,10 @@ class TaskApp:
             dialog.destroy()
             return (ret == gtk.RESPONSE_NO)
 
-    def main(self):
+    def run(self):
+        self.window.show_all()
         gtk.main()
 
 if __name__ == '__main__':
     task_app = TaskApp()
-    task_app.main()
+    task_app.run()
