fe0074afe1b5c33878eb5b12e8ac931a7dddfdc6
Alan Sparrow <alan@freeflight.org.uk>
2009-09-24 19:01:41 +0000

    Make script work for either ipaq or gary

--- src/utils/xfer_db.sh
+++ src/utils/xfer_db.sh
@@ -2,11 +2,11 @@
 
 # Dump database & copy to ipaq
 echo '.dump' | sqlite3 $HOME/.freeflight/free.db | gzip -c > free.gz
-scp free.gz ipaq:
+scp free.gz $1:
 
 # On ipaq remake database and move to correct location
-ssh ipaq 'zcat free.gz | sqlite3 free.db; rm free.gz'
-ssh ipaq 'mv free.db .freeflight'
+ssh $1 'zcat free.gz | sqlite3 free.db; rm free.gz'
+ssh $1 'mv free.db .freeflight'
 
 # Clean up
 rm free.gz
