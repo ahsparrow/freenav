1924e975fb16382c828d3b012d2014c5efd8f8f4
Alan Sparrow <github@freeflight.org.uk>
2010-06-05 13:04:27 +0100

    Update XYZ to work with gypsy

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ setup(name='freenav',
       description='FreeNav task and navigation programs',
       author='Alan Sparrow',
       scripts=['src/scripts/taskedit', 'src/scripts/freenav',
-               'src/scripts/freeconf'],
+               'src/scripts/freeconf', 'src/scripts/freexyz'],
       packages=['freenav'],
       package_dir={'freenav': 'src/freenav'},
       data_files=[('share/applications/hildon', ['data/taskedit.desktop',
                                                  'data/freenav.desktop',
-                                                 'data/freeconf.desktop']),
+                                                 'data/freeconf.desktop',
+                                                 'data/freexyz.desktop']),
                   ('share/dbus-1/services', ['data/taskedit.service',
                                              'data/freenav.service',
-                                             'data/freeconf.service'])]
+                                             'data/freeconf.service',
+                                             'data/freexyz.service'])]
      )
