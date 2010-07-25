90c81534074948069bcc99d0f31e96370217194d
Alan Sparrow <github@freeflight.org.uk>
2010-07-25 19:17:39 +0100

    Version bump to 0.1.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='freenav',
-      version='0.1.2',
+      version='0.1.3',
       description='FreeNav task and navigation programs',
       author='Alan Sparrow',
       scripts=['src/scripts/taskedit', 'src/scripts/freenav',
