83dbdc4d47e7de87451007d7ccadb0d82b27d906
Alan Sparrow <github@freeflight.org.uk>
2010-08-27 19:40:02 +0100

    Automatically set sound types from list of avaiable WAV files

--- src/freenav/freesound.py
+++ src/freenav/freesound.py
@@ -1,29 +1,30 @@
 """This module encapsulates sound handling for the freenav program"""
 
+import fnmatch
 import logging
+import os
 import os.path
 
 import pygame.mixer
-
-SOUNDS = ['sector', 'line', 'ahead', 'right-front', 'right', 'right-back',
-          'behind', 'left-back', 'left', 'left-front']
 
 class Sound():
     """Sound encapsulation class"""
     def __init__(self):
         """Initialise pygame sounds"""
         self.logger = logging.getLogger('freelog')
+        self.sounds = {}
 
         pygame.mixer.init()
+
         dir_path = os.path.join(os.getenv('HOME'), '.freeflight', 'sounds')
-        self.sounds = {}
-        for sound in SOUNDS:
-            snd_file = sound + '.wav'
-            try:
-                self.sounds[sound] = pygame.mixer.Sound(
-                        os.path.join(dir_path, snd_file))
-            except pygame.error:
-                self.logger.warning("Error loading sound %s" % sound)
+        for file in os.listdir(dir_path):
+            if fnmatch.fnmatch(file, "*.wav"):
+                sound = file.split(".")[0]
+                wav_path = os.path.join(dir_path, file)
+                try:
+                    self.sounds[sound] = pygame.mixer.Sound(wav_path)
+                except pygame.error:
+                    self.logger.warning("Error loading sound %s" % sound)
 
     def play(self, sound):
         """Play the sound"""
